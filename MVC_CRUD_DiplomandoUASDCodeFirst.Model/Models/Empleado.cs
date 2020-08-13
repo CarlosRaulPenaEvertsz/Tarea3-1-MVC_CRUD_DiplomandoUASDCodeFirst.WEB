64463df7c1ec100e59a15daf0f435d2a93af5852
CarlosRPEvertsz <carlosrpevertsz@gmail.com>
2020-08-12 21:54:58 -0400

    Add project files.

---
+++ MVC_CRUD_DiplomandoUASDCodeFirst.Model/Models/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD_DiplomandoUASDCodeFirst.Model.Models
+{
+    public class Empleado
+    {
+        public int EmpleadoID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public Nullable<System.DateTime> Fecha_Ingreso { get; set; }
+        public virtual ICollection<Registro> Registros { get; set; }
+    }
+}
